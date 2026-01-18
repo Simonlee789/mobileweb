94c22aac98f9282923188de99eb0fd5459b731e7
Simon <162421836+Simonlee789@users.noreply.github.com>
2026-01-18 19:00:32 +0700

    Lab5-fix

--- apps/lab05-photo-react/vite.config.ts
+++ apps/lab05-photo-react/vite.config.ts
@@ -3,12 +3,36 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'My Ionic App',
+        short_name: 'Ionic App',
+        description: 'An Ionic project with PWA support',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
   test: {
     globals: true,
